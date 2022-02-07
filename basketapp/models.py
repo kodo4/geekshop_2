86c261531ac220294ad68965bb798972b882bc21
Роман Кодочигов <kodochigovra@mail.ru>
2022-02-07 20:07:31 +0300

    modify basketapp models v2

--- basketapp/models.py
+++ basketapp/models.py
@@ -26,19 +26,31 @@ class Basket(models.Model):
         'return cost of all products this type'
         return self.product.price * self.quantity
 
-    @property
-    def total_quantity(self):
-        'return total quantity for user'
-        _items = Basket.objects.filter(user=self.user)
-        _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
-        return _totalquantity
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
 
-    @property
+    def total_quantity(self):
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.quantity, _items)))
+
     def total_cost(self):
-        'return total cost for user'
-        _items = Basket.objects.filter(user=self.user)
-        _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
-        return _totalcost
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.product_cost, _items)))
+
+    # @property
+    # def total_quantity(self):
+    #     'return total quantity for user'
+    #     _items = Basket.objects.filter(user=self.user)
+    #     _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
+    #     return _totalquantity
+    #
+    # @property
+    # def total_cost(self):
+    #     'return total cost for user'
+    #     _items = Basket.objects.filter(user=self.user)
+    #     _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
+    #     return _totalcost
 
     @staticmethod
     def get_item(pk):
@@ -58,14 +70,4 @@ class Basket(models.Model):
     #     self.product.save()
     #     super(self.__class__, self).save(*args, **kwargs)
 
-    @cached_property
-    def get_items_cached(self):
-        return self.user.basket.select_related()
 
-    def get_total_quantity(self):
-        _items = self.get_items_cached
-        return sum(list(map(lambda x: x.quantity, _items)))
-
-    def get_total_cost(self):
-        _items = self.get_items_cached
-        return sum(list(map(lambda x: x.product_cost, _items)))
